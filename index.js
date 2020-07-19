505e2a2783d0f7c6329fd8a7277fc682525e7c47
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2020-07-19 20:21:10 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+/* javascrpit code to run an API on gps location. Non handling of errors.
+Running on ES6.
+ */
+
+ getLocation = () => {
+     if(navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(position => {
+             console.log(`Longitude: ${position.coords.longitude}`);
+             console.log(`Latitude: ${position.coords.latitude}`);
+         });
+     }
+     else {
+         console.log(`geolocation is not supported by this browser.`);
+     }
+ }
+ getLocation();
