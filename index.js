381a8fb5450c4ff3187c52683fb56b887e87e79f
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2021-12-25 13:23:37 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ Running on ES6.
  getLocation = () => {
      if (navigator.geolocation) {
          navigator.geolocation.getCurrentPosition(position => {
-             console.log(`Longitude: ${position.coords.longitude}`);
-             console.log(`Latitude: ${position.coords.latitude}`);
+         console.log(`Longitude: ${position.coords.longitude}`);
+         console.log(`Latitude: ${position.coords.latitude}`);
          });
-     }
-     else {
+     } else {
          console.log("Geolocation is not supported by this browser.");
      }
- }
+ };
+ 
  getLocation();
